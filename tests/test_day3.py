24cff9fe66e57f5a839adb4c8113ef40329242c4
LouisStefanuto <louis.stefanuto@gmail.com>
2024-12-03 14:35:35 +0100

    add: second half

--- tests/test_day3.py
+++ tests/test_day3.py
@@ -1,4 +1,16 @@
-from aoc2024.day3 import extract_digits, get_valid_operations, mul
+from aoc2024.day3 import (
+    clean_operations,
+    concatenate_strings,
+    extract_digits,
+    get_valid_operations,
+    mul,
+)
+
+
+def test_concatenate_strings():
+    inputs = ["aaaa", "bbbb" "cccc"]
+    expected = "aaaabbbbcccc"
+    assert concatenate_strings(inputs) == expected
 
 
 def test_get_valid_operations():
@@ -8,9 +20,15 @@ def test_get_valid_operations():
 
 
 def test_extract_digits():
-    inputs = ["mul(2,4)"]
+    inputs = "mul(2,4)"
     expected = (2, 4)
     assert extract_digits(inputs) == expected
+
+
+def test_clean_operations():
+    inputs = ["mul(2,4)", "don't()", "mul(5,5)", "mul(11,8)", "do()", "mul(8,5)"]
+    expected = ["mul(2,4)", "mul(8,5)"]
+    assert clean_operations(inputs) == expected
 
 
 def test_mul():
