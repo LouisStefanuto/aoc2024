e21b8c311abb27c328ce77b4107f9849c3b15e61
LouisStefanuto <louis.stefanuto@gmail.com>
2024-12-24 14:11:16 +0100

    add: day 19 half 2

--- aoc2024/day19.py
+++ aoc2024/day19.py
@@ -14,6 +14,7 @@ While the list contains designs:
 return False
 """
 
+from functools import cache
 from pathlib import Path
 
 from aoc2024.day4 import read_file_to_list
@@ -24,7 +25,7 @@ def parse_input(file: Path) -> tuple[list[str], list[str]]:
     return data[0].split(", "), data[2:]  # patterns, designs
 
 
-def solve(patterns: list[str], designs: list[str]) -> int:
+def solve1(patterns: list[str], designs: list[str]) -> int:
     craftable_patterns = [is_craftable(design, patterns) for design in designs]
     return sum(craftable_patterns)
 
@@ -46,6 +47,24 @@ def is_craftable(design: str, patterns: list[str]) -> bool:
     return False
 
 
+def solve2(patterns: tuple[str, ...], designs: tuple[str, ...]) -> int:
+    craftable_patterns = [get_num_crafts_v2(design, patterns) for design in designs]
+    return sum(craftable_patterns)
+
+
+@cache
+def get_num_crafts_v2(design: str, patterns: tuple[str]) -> int:
+    if len(design) == 0:
+        return 1
+
+    return sum(
+        get_num_crafts_v2(design[len(pattern) :], patterns)
+        for pattern in patterns
+        if design.startswith(pattern)
+    )
+
+
 if __name__ == "__main__":
     patterns, designs = parse_input(Path("inputs/day19.txt"))
-    print("Result1:", solve(patterns, designs))
+    print("Result1:", solve1(patterns, designs))
+    print("Result2:", solve2(tuple(patterns), tuple(designs)))
--- tests/test_day19.py
+++ tests/test_day19.py
@@ -1,12 +1,18 @@
 import pytest
 
-from aoc2024.day19 import solve
+from aoc2024.day19 import solve1, solve2
 
 
-def test_solve():
+def test_solve1():
     patterns = ["r", "wr", "b", "g", "bwu", "rb", "gb", "br"]
     designs = ["brwrr", "bggr", "gbbr", "rrbgbr", "ubwu", "bwurrg", "brgr", "bbrgwb"]
-    assert solve(patterns, designs) == 6
+    assert solve1(patterns, designs) == 6
+
+
+def test_solve2():
+    patterns = ("r", "wr", "b", "g", "bwu", "rb", "gb", "br")
+    designs = ("brwrr", "bggr", "gbbr", "rrbgbr", "ubwu", "bwurrg", "brgr", "bbrgwb")
+    assert solve2(patterns, designs) == 16
 
 
 if __name__ == "__main__":
